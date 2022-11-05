0552b9e89dbca885c3882c36f49ce89c5fa8992f
Jeff Zohrab <jzohrab@gmail.com>
2022-11-05 10:37:16 -0600

    Dev scripts to kill tbpref.

---
+++ devscripts/remove_tbpref/find_tbpref.sh
@@ -0,0 +1,1 @@
+find . -name '*.*' -print0 | xargs -0 grep -i tbpref 2>/dev/null
---
+++ devscripts/remove_tbpref/run_seds.sh
@@ -0,0 +1,15 @@
+set +x
+
+FILES=`find . -name '*.*' -print0 | xargs -0 grep -l tbpref 2>/dev/null`;
+FILES=`echo ./edit_words.php`
+
+for f in $FILES; do
+    echo "$f"
+
+    for tbl in archivedtexts archtexttags feedlinks languages newsfeeds sentences settings tags tags2 temptextitems tempwords textitems2 texts texttags tts words wordtags; do
+        echo "  $tbl"
+        sed -i "" "s/' \. \$tbpref \. '$tbl/$tbl/g" $f
+        sed -i "" "s/\" \. \$tbpref \. \"$tbl/$tbl/g" $f
+        sed -i "" "s/{\$tbpref}$tbl/$tbl/g" $f
+    done
+done
