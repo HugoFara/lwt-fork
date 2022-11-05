fb599b9ab53a8017498b9060ffcae480e0620c91
Jeff Zohrab <jzohrab@gmail.com>
2022-11-05 11:12:47 -0600

    Tweaks.

--- devscripts/remove_tbpref/find_tbpref.sh
+++ devscripts/remove_tbpref/find_tbpref.sh
@@ -1,1 +1,1 @@
-find . -name '*.*' -print0 | xargs -0 grep -i tbpref 2>/dev/null
+find . -name '*.php' -print0 | xargs -0 grep -i tbpref 2>/dev/null
--- devscripts/remove_tbpref/run_seds.sh
+++ devscripts/remove_tbpref/run_seds.sh
@@ -1,14 +1,14 @@
 # set -e
 # set -x
 
-FILES=`find . -name '*.*' -print0 | xargs -0 grep -l tbpref 2>/dev/null`;
+FILES=`find . -name '*.php' -print0 | xargs -0 grep -l tbpref 2>/dev/null`;
 # FILES=`echo ./inc/database_connect.php`
 echo $FILES
 
 for f in $FILES; do
     echo "$f"
 
-    for tbl in archivedtexts archtexttags feedlinks languages merge_words newsfeeds numbers sentences settings tags tags2 temptextitems tempwords textitems2 texts texttags tts words wordtags; do
+    for tbl in archivedtexts archtexttags feedlinks languages merge_words newsfeeds numbers sentences settings tags tags2 temptextitems tempwords textitems textitems2 texts texttags tts words wordtags; do
         # echo "  $tbl"
         sed -i "" "s/' \. \$tbpref \. '$tbl/$tbl/g" $f
         sed -i "" "s/\" \. \$tbpref \. \"$tbl/$tbl/g" $f
