0f5776afcae971caa6d6b643ab738c62cbfedd18
Jeff Zohrab <jzohrab@gmail.com>
2022-11-05 17:51:51 -0600

    wip script to remove global tbpref stuff when possible

---
+++ devscripts/remove_tbpref/only_global_or_comment.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Find files with tbpref but where the line either matches
+#  * @global string $tbpref Database table prefix
+#  global $tbpref;
+#  global $tbpref, $debug;
+# if the line is anything different, the file can't be updated, otherwise it can.
+
+# Obligatory "I hate bash" note here.
+
+# FILES=`find . -name '*.php' -print0 | xargs -0 grep -l tbpref 2>/dev/null`;
+# echo $FILES
+FILES=`echo ./edit_texts.php`
+
+for f in $FILES; do
+    echo "$f"
+
+    echo "initial grep"
+    grep "\$tbpref" $f
+    echo "----------------"
+    LINES=$(grep "\$tbpref" $f | sed -Ee 's/^[[:space:]]*//g' | sed -Ee 's/[[:space:]]*$//g' | sort | uniq)
+
+    HASBADLINE=0
+    BADLINE=""
+    while IFS= read -r line; do
+        echo "=> $line"
+        OK=0
+        if [[ "$line" =~ "@global string $tbpref" ]]; then
+            OK=1
+        fi
+        if [[ "$line" =~ "global $tbpref" ]]; then
+            OK=1
+        fi
+        if [[ "$line" =~ "global $tbpref, $debug" ]]; then
+            OK=1
+        fi
+
+        if [[ $OK -eq 1 ]]; then
+            echo "ok, this line matches one of the patterns"
+        else
+            echo "Bad line: ${line}"
+            BADLINE="$line"
+            HASBADLINE=1
+        fi
+    done <<< "$LINES"
+
+    if [[ $HASBADLINE -eq 0 ]]; then
+        echo "File only contains lines matching patterns, can clean it."
+    else
+        echo "Can't clean file, has bad line ${BADLINE}"
+    fi
+done
