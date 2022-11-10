1dfb02b0a7e95b9c9d3ad99d95a868c59764341e
Jeff Zohrab <jzohrab@gmail.com>
2022-11-10 12:37:37 -0600

    Extract function show_form() for new terms.

--- edit_word.php
+++ edit_word.php
@@ -254,6 +254,59 @@ class FormData
 }
 
 
+function show_form($formdata)
+{
+?>
+ <form name="newword" class="validate" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+ <input type="hidden" name="fromAnn" value="<?php echo $formdata->fromAnn; ?>" />
+ <input type="hidden" name="WoLgID" id="langfield" value="<?php echo $formdata->lang; ?>" />
+ <input type="hidden" name="WoTextLC" value="<?php echo tohtml($formdata->termlc); ?>" />
+ <input type="hidden" name="tid" value="<?php echo $_REQUEST['tid']; ?>" />
+ <input type="hidden" name="ord" value="<?php echo $_REQUEST['ord']; ?>" />
+ <table class="tab2" cellspacing="0" cellpadding="5">
+ <tr title="Only change uppercase/lowercase!">
+ <td class="td1 right"><b>New Term:</b></td>
+ <td class="td1"><input <?php echo $formdata->srcdir; ?> class="notempty checkoutsidebmp" data_info="New Term" type="text" name="WoText" id="wordfield" value="<?php echo tohtml($formdata->term); ?>" maxlength="250" size="35" /> <img src="icn/status-busy.png" title="Field must not be empty" alt="Field must not be empty" />
+ </td></tr>
+        <?php print_similar_terms_tabrow(); ?>
+ <tr>
+ <td class="td1 right">Translation:</td>
+ <td class="td1"><textarea name="WoTranslation" class="setfocus textarea-noreturn checklength checkoutsidebmp" data_maxlength="500" data_info="Translation" cols="35" rows="3"></textarea></td>
+ </tr>
+ <tr>
+ <td class="td1 right">Tags:</td>
+ <td class="td1">
+        <?php echo getWordTags(0); ?>
+ </td>
+ </tr>
+ <tr>
+ <td class="td1 right">Romaniz.:</td>
+ <td class="td1"><input type="text" class="checkoutsidebmp" data_info="Romanization" name="WoRomanization" value="" maxlength="100" size="35" /></td>
+ </tr>
+ <tr>
+ <td class="td1 right">Sentence<br />Term in {...}:</td>
+ <td class="td1"><textarea <?php echo $scrdir; ?> name="WoSentence" class="textarea-noreturn checklength checkoutsidebmp" data_maxlength="1000" data_info="Sentence" cols="35" rows="3"><?php echo tohtml($formdata->sentence); ?></textarea></td>
+ </tr>
+        <?php print_similar_terms_tabrow(); ?>
+ <tr>
+ <td class="td1 right">Status:</td>
+ <td class="td1">
+        <?php echo get_wordstatus_radiooptions($formdata->status); ?>
+ </td>
+ </tr>
+ <tr>
+ <td class="td1 right" colspan="2">
+        <?php echo createDictLinksInEditWin($formdata->lang, $formdata->term, 'document.forms[0].WoSentence', isset($_GET['nodict'])?0:1); ?>
+ &nbsp; &nbsp; &nbsp; <input type="submit" name="op" value="Save" /></td>
+ </tr>
+ </table>
+ </form>
+ <div id="exsent"><span class="click" onclick="do_ajax_show_sentences(<?php echo $formdata->lang; ?>, <?php echo prepare_textdata_js($formdata->termlc) . ', ' . prepare_textdata_js("document.forms['newword'].WoSentence") . ', 0'; ?>);"><img src="icn/sticky-notes-stack.png" title="Show Sentences" alt="Show Sentences" /> Show Sentences</span></div>    
+
+        <?php
+}
+
+
 $fromAnn = getreq("fromAnn"); // from-recno or empty
 $lang = null;
 $term = null;
@@ -304,54 +357,7 @@ if (isset($_REQUEST['op'])) {
         $formdata->status = 1;
         $formdata->status_old = 1;
 
-?>
- <form name="newword" class="validate" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
- <input type="hidden" name="fromAnn" value="<?php echo $formdata->fromAnn; ?>" />
- <input type="hidden" name="WoLgID" id="langfield" value="<?php echo $formdata->lang; ?>" />
- <input type="hidden" name="WoTextLC" value="<?php echo tohtml($formdata->termlc); ?>" />
- <input type="hidden" name="tid" value="<?php echo $_REQUEST['tid']; ?>" />
- <input type="hidden" name="ord" value="<?php echo $_REQUEST['ord']; ?>" />
- <table class="tab2" cellspacing="0" cellpadding="5">
- <tr title="Only change uppercase/lowercase!">
- <td class="td1 right"><b>New Term:</b></td>
- <td class="td1"><input <?php echo $formdata->scrdir; ?> class="notempty checkoutsidebmp" data_info="New Term" type="text" name="WoText" id="wordfield" value="<?php echo tohtml($formdata->term); ?>" maxlength="250" size="35" /> <img src="icn/status-busy.png" title="Field must not be empty" alt="Field must not be empty" />
- </td></tr>
-        <?php print_similar_terms_tabrow(); ?>
- <tr>
- <td class="td1 right">Translation:</td>
- <td class="td1"><textarea name="WoTranslation" class="setfocus textarea-noreturn checklength checkoutsidebmp" data_maxlength="500" data_info="Translation" cols="35" rows="3"></textarea></td>
- </tr>
- <tr>
- <td class="td1 right">Tags:</td>
- <td class="td1">
-        <?php echo getWordTags(0); ?>
- </td>
- </tr>
- <tr>
- <td class="td1 right">Romaniz.:</td>
- <td class="td1"><input type="text" class="checkoutsidebmp" data_info="Romanization" name="WoRomanization" value="" maxlength="100" size="35" /></td>
- </tr>
- <tr>
- <td class="td1 right">Sentence<br />Term in {...}:</td>
- <td class="td1"><textarea <?php echo $scrdir; ?> name="WoSentence" class="textarea-noreturn checklength checkoutsidebmp" data_maxlength="1000" data_info="Sentence" cols="35" rows="3"><?php echo tohtml($formdata->sentence); ?></textarea></td>
- </tr>
-        <?php print_similar_terms_tabrow(); ?>
- <tr>
- <td class="td1 right">Status:</td>
- <td class="td1">
-        <?php echo get_wordstatus_radiooptions($formdata->status); ?>
- </td>
- </tr>
- <tr>
- <td class="td1 right" colspan="2">
-        <?php echo createDictLinksInEditWin($formdata->lang, $formdata->term, 'document.forms[0].WoSentence', isset($_GET['nodict'])?0:1); ?>
- &nbsp; &nbsp; &nbsp; <input type="submit" name="op" value="Save" /></td>
- </tr>
- </table>
- </form>
- <div id="exsent"><span class="click" onclick="do_ajax_show_sentences(<?php echo $formdata->lang; ?>, <?php echo prepare_textdata_js($formdata->termlc) . ', ' . prepare_textdata_js("document.forms['newword'].WoSentence") . ', 0'; ?>);"><img src="icn/sticky-notes-stack.png" title="Show Sentences" alt="Show Sentences" /> Show Sentences</span></div>    
-
-        <?php
+        show_form($formdata);
         
     } else {
         // CHG
